58f8b1047e5080814b1c2dad3217f4eb12268fe9
Emily Chao <elchao96@live.com>
2015-04-14 08:14:06 -0500

    merging in brianna's stuff

--- adminmovie.php
+++ adminmovie.php
@@ -5,43 +5,43 @@
 <table>
 <tr>
 <td><div class="input-group">
-  <input type="text" class="form-control" placeholder="Title (required)" aria-describedby="basic-addon1">
+  <input type="text" class="form-control" placeholder="Title (required)" aria-describedby="basic-addon1" data-bind="value: adminmovietitle">
 </div></td>
 </tr>
 <tr>
 <td><div class="input-group">
-  <input type="text" class="form-control" placeholder="Rating" aria-describedby="basic-addon1">
+  <input type="text" class="form-control" placeholder="Rating" aria-describedby="basic-addon1" data-bind="value: adminmovierating">
 </div></td>
 </tr>
 <tr>
 <td><div class="input-group">
-  <input type="text" class="form-control" placeholder="Box Office (in millions)" aria-describedby="basic-addon1">
+  <input type="text" class="form-control" placeholder="Box Office (in millions)" aria-describedby="basic-addon1" data-bind="value: adminmovieboxoffice">
 </div></td>
 </tr>
 <tr>
 <td><div class="input-group">
-  <input type="text" class="form-control" placeholder="Budget (in millions)" aria-describedby="basic-addon1">
+  <input type="text" class="form-control" placeholder="Budget (in millions)" aria-describedby="basic-addon1" data-bind="value: adminmoviebudget">
 </div></td>
 </tr>
 <tr>
 <td><div class="input-group">
-  <input type="text" class="form-control" placeholder="Year Nominated" aria-describedby="basic-addon1">
+  <input type="text" class="form-control" placeholder="Year Nominated" aria-describedby="basic-addon1" data-bind="value: adminmovieyearnom">
 </div></td>
 </tr>
 <tr>
 <td><div class="input-group">
-  <input type="text" class="form-control" placeholder="Place Filmed" aria-describedby="basic-addon1">
+  <input type="text" class="form-control" placeholder="Place Filmed" aria-describedby="basic-addon1" data-bind="value: adminmovieplacefilmed">
 </div></td>
 </tr>
 <tr>
 <td><div class="input-group">
-  <input type="text" class="form-control" placeholder="Movie Fictional Location" aria-describedby="basic-addon1">
+  <input type="text" class="form-control" placeholder="Movie Fictional Location" aria-describedby="basic-addon1" data-bind="value: adminmovieficlocation">
 </div></td>
 </tr>
 <tr><td>Hi</td></tr>
 <tr>
 <td><div class="btn-group" role="group" aria-label="...">
-<button type="button" class="btn btn-default" block><a href="#">Insert into database!</a></button>
+<button type="button" class="btn btn-default" data-bind="click: postadminMovieToDB" block>Insert into database!</button>
 </div></td>
 </tr>
 </table>
--- admintv.php
+++ admintv.php
@@ -5,48 +5,58 @@
 <table>
 <tr>
 <td><div class="input-group">
-  <input type="text" class="form-control" placeholder="Title (required)" aria-describedby="basic-addon1">
+  <input type="text" class="form-control" placeholder="Title (required)" aria-describedby="basic-addon1" data-bind="value: admintvtitle">
 </div></td>
 </tr>
 <tr>
 <td><div class="input-group">
-  <input type="text" class="form-control" placeholder="Number of Episodes" aria-describedby="basic-addon1">
+  <input type="text" class="form-control" placeholder="Number of Episodes" aria-describedby="basic-addon1" data-bind="value: admintvnumepisodes">
 </div></td>
 </tr>
 <tr>
 <td><div class="input-group">
-  <input type="text" class="form-control" placeholder="Number of Seasons" aria-describedby="basic-addon1">
+  <input type="text" class="form-control" placeholder="Number of Seasons" aria-describedby="basic-addon1" data-bind="value: admintvnumseasons">
 </div></td>
 </tr>
 <tr>
 <td><div class="input-group">
-  <input type="text" class="form-control" placeholder="Still running?" aria-describedby="basic-addon1">
+  <input type="text" class="form-control" placeholder="Still running?" aria-describedby="basic-addon1" data-bind="value: admintvstillrunning">
 </div></td>
 </tr>
 <tr>
 <td><div class="input-group">
-  <input type="text" class="form-control" placeholder="Network" aria-describedby="basic-addon1">
+  <input type="text" class="form-control" placeholder="Network" aria-describedby="basic-addon1" data-bind="value: admintvnetwork">
 </div></td>
 </tr>
 <tr>
 <td><div class="input-group">
-  <input type="text" class="form-control" placeholder="Camera Setup" aria-describedby="basic-addon1">
+  <input type="text" class="form-control" placeholder="Camera Setup" aria-describedby="basic-addon1" data-bind="value: admintvcamerasetup">
 </div></td>
 </tr>
 <tr>
 <td><div class="input-group">
-  <input type="text" class="form-control" placeholder="Minimum Runtime" aria-describedby="basic-addon1">
+  <input type="text" class="form-control" placeholder="Minimum Runtime" aria-describedby="basic-addon1" data-bind="value: admintvminruntime">
 </div></td>
 </tr>
 <tr>
 <td><div class="input-group">
-  <input type="text" class="form-control" placeholder="Maximum Runtime" aria-describedby="basic-addon1">
+  <input type="text" class="form-control" placeholder="Maximum Runtime" aria-describedby="basic-addon1" data-bind="value: admintvmaxruntime">
+</div></td>
+</tr>
+<tr>
+<td><div class="input-group">
+  <input type="text" class="form-control" placeholder="Place Filmed" aria-describedby="basic-addon1" data-bind="value: admintvplacefilmed">
+</div></td>
+</tr>
+<tr>
+<td><div class="input-group">
+  <input type="text" class="form-control" placeholder="TV Show Fictional Location" aria-describedby="basic-addon1" data-bind="value: admintvficlocation">
 </div></td>
 </tr>
 <tr><td>Hi</td></tr>
 <tr>
 <td><div class="btn-group" role="group" aria-label="...">
-<button type="button" class="btn btn-default" block><a href="#">Insert into database!</a></button>
+<button type="button" class="btn btn-default" data-bind="click: postadminTvToDB" block>Insert into database!</button>
 </div></td>
 </tr>
 </table>
