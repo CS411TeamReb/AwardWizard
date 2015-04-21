09133542228618c9204d172b64afea60fc7bf7e9
Emily Chao <elchao96@live.com>
2015-04-21 17:55:24 -0500

    things

--- index.php
+++ index.php
@@ -1,7 +1,55 @@
 <?php
 	include 'layout/header.php'
 ?>
-
+<div id="home">
+	<div class="t">
+		<div class="main">
+			<div class="row homeText">
+				<h1 align="center">AWARD WIZARD</h1>
+			</div>
+			<div class="row homeText">
+				<div class="col-md-3">
+					<h4>Great Resource for Entertainment</h4>
+					<p>Bringing together all of the entertainment data that is scattered throughout the web into one clean interface.</p>
+				</div>
+				<div class="col-md-3">
+					<h4>Available Functionality</h4>
+					<ul>
+						<li>Users - power to insert your own favorites</li>
+						<li>Admin - power to insert, delete, update new data within our databases</li>
+						<li>Run searches on dates, numerics and string values within our datasets</li>
+						<li>See visualizations and correlations between different data sets, presented in charts, graphs and maps</li>
+					</ul>
+				</div>
+				<div class="col-md-3">
+					<h4>Contributors</h4>
+					<ul>
+						<li>Emily Chao</li>
+						<li>Robin Sturm</li>
+						<li>Brianna Ifft </li>
+						<li>Milan Dasgupta </li>
+					<p></p>
+				</div>
+				<div class="col-md-3">
+					<h4>Sources</h4>
+					<ul>
+						<li>PlaybillStore.com</li>
+						<li>PlaybillVault.com</li>
+						<li>Wikipedia</li>
+						<li>BroadwayWorld.com</li>
+						<li>Playbill.com</li>
+						<li>CriticalJazz.com</li>
+						<li>Amazon.com</li>
+						<li>thisisrnb.com</li>
+						<li>IMDb</li>
+						<li>Cookindineout.com</li>
+						<li>IBDb</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 <?php
 	include 'layout/footer.php'
 ?>
